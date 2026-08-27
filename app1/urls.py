66c6af18ce174371b025867ef2ec5ae1c4ff9e94
Baaleria <andrea.lara06@inacapmail.cl>
2026-08-27 12:23:11 -0400

    Mejora de URL

--- app1/urls.py
+++ app1/urls.py
@@ -3,5 +3,5 @@ from . import views # . significa que importa views desde la ruta actual
 
 urlpatterns = [
     path('v1/', views.Inicio),
-    path('v2/', views.Inicio)
+    path('v3/', views.Inicio)
 ]
