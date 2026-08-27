0d19da38c3e06b8a05a4dfd3c241204ef25a9332
Baaleria <andrea.lara06@inacapmail.cl>
2026-08-27 12:02:24 -0400

    Actualización

---
+++ app1/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views # . significa que importa views desde la ruta actual
+
+urlpatterns = [
+    path('v1/', views.Inicio),
+    path('v2/', views.Inicio)
+]
